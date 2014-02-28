9a4e5669c4fff5957e16f432b8965f615e7076b8
John Bohn <jjbohn@gmail.com>
2014-02-28 11:11:14 -0600

    Alphabetize requires

--- lib/openid_connect.rb
+++ lib/openid_connect.rb
@@ -1,7 +1,7 @@
 require "oauth2"
-require "openid_connect/version"
 require "openid_connect/access_token"
 require "openid_connect/client"
+require "openid_connect/version"
 
 module OpenIDConnect
 end
